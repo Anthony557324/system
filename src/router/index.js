fe72cdd244e2fcaabb238861fa6c6338ad2cb923
Anthony557324 <1621860255@qq.com>
2023-08-11 21:27:50 +0800

    feat:commit

---
+++ src/router/index.js
@@ -0,0 +1,18 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+import HomeComp from '@/components/Home.vue'
+import LoginComp from '@/components/Login.vue'
+import MainComp from '@/components/Main.vue'
+
+const routes = [
+    { path: '/', component: MainComp },
+    { path: '/login', component: LoginComp },
+    { path: '/home', components: HomeComp }
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes, // `routes: routes`的缩写
+})
+
+export default router
