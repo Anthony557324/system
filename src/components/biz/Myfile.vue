e26f50f6a948de9883596364060e51e383b7ca9f
Anthony557324 <1621860255@qq.com>
2023-08-13 16:27:12 +0800

    feat:目录调整

---
+++ src/components/biz/Myfile.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>我的文件</div>
+</template>
+<script>
+export default {
+    name:'MyfileComp',
+    setup(){}
+}
+</script>
---
+++ src/components/biz/Upload.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>上传文件</div>
+</template>
+<script>
+export default {
+    name: 'UploadComp',
+    setup() { }
+}
+</script>
