6966e85b58da42dc6895049a8f7029c0ad2bc51c
Anthony557324 <1621860255@qq.com>
2023-09-09 10:57:56 +0800

    feat:文件传输

--- src/components/biz/Myfile.vue
+++ src/components/biz/Myfile.vue
@@ -1,10 +1,32 @@
 <template>
-  <div>我的文件</div>
+  <el-button @click="upload">上传文件</el-button>
+  <input @change="handleFile" ref="file" type="file" style="width: 0;height: 0;"/>
+  <div class="tag" v-show="filename">
+    <el-tag>{{ filename }}</el-tag>
+  </div>
 </template>
 
 <script>
+import { ref } from "vue";
 export default {
   name: 'MyfileComp',
-  setup() {}
+  setup() {
+    const file = ref(null)
+    const filename = ref('')
+    function handleFile(e) {
+      console.log(e.target.files);
+      console.log(file.value.files);
+      filename.value = file.value.files[0].name
+    }
+    function upload() {
+      file.value.click()
+    }
+    return { file, filename, upload, handleFile }
+  }
 }
 </script>
+<style scoped>
+.tag {
+  margin-top: 10px;
+}
+</style>
