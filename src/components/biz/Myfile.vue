40e8b17fbc6a1d51eca90ac6dbcea6e4ab1441ea
Anthony557324 <1621860255@qq.com>
2023-09-09 16:39:34 +0800

    feat:文件上传修改

--- src/components/biz/Myfile.vue
+++ src/components/biz/Myfile.vue
@@ -1,27 +1,55 @@
 <template>
-  <el-button @click="upload">上传文件</el-button>
-  <input @change="handleFile" ref="file" type="file" style="width: 0;height: 0;"/>
+  <el-button @click="upload">选择文件</el-button>
+  <input @change="handleFile" ref="file" type="file" style="width: 0;height: 0;" />
   <div class="tag" v-show="filename">
     <el-tag>{{ filename }}</el-tag>
   </div>
+  <div>
+    <el-button @click="testServer">上传文件</el-button>
+  </div>
+  <div>
+    <el-button @click="uploadFileBase64">base64上传</el-button>
+  </div>
+  <img src="">
 </template>
 
 <script>
 import { ref } from "vue";
+import { selfServer } from '@/http/axios.js'
 export default {
   name: 'MyfileComp',
   setup() {
     const file = ref(null)
     const filename = ref('')
+
+    // reader
+    const base64Reader = new FileReader()
+    function getReaderResult() {
+
+    }
+    // 事件注册的同时，要考虑事件卸载时机
+
+    const formData = new FormData()
     function handleFile(e) {
       console.log(e.target.files);
       console.log(file.value.files);
       filename.value = file.value.files[0].name
+      formData.append('file', file.value.files[0])
     }
     function upload() {
       file.value.click()
     }
-    return { file, filename, upload, handleFile }
+
+    // formData 表单上传文件
+    function testServer() {
+      selfServer.post('/upload', formData)
+    }
+
+    // base64 编码
+    function uploadFileBase64() {
+
+    }
+    return { file, filename, upload, handleFile, testServer, uploadFileBase64 }
   }
 }
 </script>
---
+++ src/http/axios.js
@@ -0,0 +1,24 @@
+import axios from 'axios'
+
+const gaodeHttp = axios.create({
+  baseURL: 'https://restapi.amap.com/v3',
+  timeout: 1000,
+})
+
+const MockHttp = axios.create({
+  baseURL: 'http://127.0.0.1:4523/m1/3149923-0-default',
+  timeout: 1000,
+})
+
+const selfServer = axios.create({
+  baseURL: 'http://localhost:8080',
+  headers: {
+    'ljq': 'asf'
+  }
+})
+
+export {
+  gaodeHttp,
+  MockHttp as http,
+  selfServer
+}
