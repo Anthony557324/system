d8f5844ad8a5e6fe324a600b21ab0a5f868f83f5
Anthony557324 <1621860255@qq.com>
2023-09-10 16:08:44 +0800

    feat:广告页修改

--- src/components/layout/Mainbody.vue
+++ src/components/layout/Mainbody.vue
@@ -1,26 +1,20 @@
 <template>
-    <div class="container">
-      <el-button @click="login">跳转登录</el-button>
-    </div>
-  </template>
-  <script>
-  import { useRouter } from 'vue-router';
-  export default {
-    name: 'MainbodyComp',
-    setup() {
-      const router = useRouter()
-        function login() {
-            router.push({ name: 'login' })
-        }
-        return {
-            login
-        }
-    }
-  }
-  </script>
-  <style scoped>
-  .container {
-    height: calc(100%-5.5rem);
-    overflow-y: auto;
-  }
-  </style>
+  <div class="container">
+    <img class='background' src="https://images.pexels.com/photos/414144/pexels-photo-414144.jpeg?auto=compress&cs=tinysrgb&w=600" alt="广告页背景图">
+  </div>
+</template>
+<script>
+export default {
+  name: 'MainbodyComp',
+  setup() {}
+}
+</script>
+<style scoped>
+.container {
+  height: calc(100%-5.5rem);
+  overflow-y: auto;
+}
+.background{
+  width: 100%;
+}
+</style>
--- src/components/layout/Mainheader.vue
+++ src/components/layout/Mainheader.vue
@@ -1,14 +1,36 @@
 <template>
-    <div class="container"></div>
-    </template>
-    <script>
-    export default {
-      name: 'MainheaderComp',
-      setup() {}
+  <div class="container">
+    <div class="menu">图床系统</div>
+    <el-button @click="login" class="loginbutton">登录</el-button>
+  </div>
+</template>
+<script>
+import { useRouter } from 'vue-router';
+export default {
+  name: 'MainheaderComp',
+  setup() {
+    const router = useRouter()
+    function login() {
+      router.push({ name: 'login' })
     }
-    </script>
-    <style scoped>
-    .container {
-      height: 5.5rem;
+    return { 
+      login 
     }
-    </style>
+  }
+}
+</script>
+<style scoped>
+.container {
+  height: 5.5rem;
+  display:flex;
+  align-items: center;
+}
+.menu{
+  flex-grow: 1;
+  font-size: 1.5rem;
+  margin-left: 2rem;
+}
+.loginbutton{
+  margin-right: 2rem;
+}
+</style>
